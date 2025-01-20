3f00b5b4cb9dfd86b0aa5429bc773de67cd6294c
KMS <y.dudar@list.ru>
2025-01-20 14:53:40 +0300

    fix add key prop

--- hw-2/src/main.jsx
+++ hw-2/src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { Fragment } from "react";
 
 const rootElement = document.querySelector("#root");
 
@@ -10,25 +11,33 @@ reactRoot.render(
   <>
     {restaurants.map((restaurant) => {
       return (
-        <>
+        <Fragment key={restaurant.id}>
           <h2>{restaurant.name}</h2>
 
           <h3>Меню</h3>
 
           <ul>
-            {restaurant.menu.map(({ name }) => {
-              return <li>{name}</li>;
+            {restaurant.menu.map(({ name, id }) => {
+              return (
+                <Fragment key={id}>
+                  <li>{name}</li>
+                </Fragment>
+              );
             })}
           </ul>
 
           <h3>Отзывы</h3>
 
           <ul>
-            {restaurant.reviews.map(({ text }) => {
-              return <li>{text}</li>;
+            {restaurant.reviews.map(({ text, id }) => {
+              return (
+                <Fragment key={id}>
+                  <li>{text}</li>
+                </Fragment>
+              );
             })}
           </ul>
-        </>
+        </Fragment>
       );
     })}
   </>
