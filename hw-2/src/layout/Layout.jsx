e327b4afe137e855b265ff76a01fd0193f31ac76
KMS <y.dudar@list.ru>
2025-01-25 01:02:12 +0300

    add components Layout, Footer, Header

---
+++ hw-2/src/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import { Footer } from "../footer/Footer";
+import { Header } from "../header/Header";
+
+export const Layout = ( {children} ) => {
+    return (
+        <>
+            <Header />
+            <main>
+                {children}
+            </main>
+            <Footer /> 
+        </>
+    );
+};
--- hw-2/src/main.jsx
+++ hw-2/src/main.jsx
@@ -1,34 +1,7 @@
-import { createRoot } from "react-dom/client";
-import { Fragment } from "react";
-import { restaurants } from "../materials/mock";
+import { createRoot } from "react-dom/client";;
+import { App } from "./app/App";
 
 const rootElement = document.querySelector("#root");
 const reactRoot = createRoot(rootElement);
 
-reactRoot.render(
-  <>
-    {restaurants.map(({ id, name, menu, reviews }) => {
-      return (
-        <Fragment key={id}>
-          <h2>{name}</h2>
-
-          <h3>Меню</h3>
-
-          <ul>
-            {menu.map(({ name, id }) => {
-              return <li key={id}>{name}</li>;
-            })}
-          </ul>
-
-          <h3>Отзывы</h3>
-
-          <ul>
-            {reviews.map(({ text, id }) => {
-              return <li key={id}>{text}</li>;
-            })}
-          </ul>
-        </Fragment>
-      );
-    })}
-  </>
-);
+reactRoot.render( <App/> );
